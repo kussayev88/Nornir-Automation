2d43e75674730762a67acc65e68a546b34a6aedc
Bauyrzhan Kussayev <kussayev88@gmail.com>
2024-05-23 05:59:58 +0000

    Deleted unnessecary files

--- show_run.py
+++
@@ -1,7 +0,0 @@
-from nornir import InitNornir
-from nornir_scrapli.tasks import send_command
-from nornir_utils.plugins.functions import print_result
-
-nr = InitNornir(config_file="config.yaml")
-result = nr.run(task=send_command, command = "show int status")
-print_result(result)
